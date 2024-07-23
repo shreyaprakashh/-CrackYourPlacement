8314a7799dfaacbab12686492a8011805df0a223
Shreya Prakash <101544747+shreyaprakashh@users.noreply.github.com>
2024-07-24 00:06:11 +0530

    Update DAY7_PrintAllDuplicateCharactersInAString.java

--- DAY7_PrintAllDuplicateCharactersInAString.java
+++ DAY7_PrintAllDuplicateCharactersInAString.java
@@ -1,1 +1,21 @@
 
+class Solution
+{
+  
+  
+  static void printDups(String str)
+  {
+    Map<Character, Integer> count = new HashMap<>();
+    for (int i = 0; i < str.length(); i++) {
+      if(count.containsKey(str.charAt(i)))
+        count.put(str.charAt(i) , count.get(str.charAt(i))+1); 
+      else count.put(str.charAt(i),1);
+      
+    }
+
+    for (Map.Entry<Character,Integer> mapElement : count.entrySet()) {   
+      if (mapElement.getValue() > 1)   
+        return (mapElement.getKey() + ", count = " + mapElement.getValue());
+    }
+  }
+}
