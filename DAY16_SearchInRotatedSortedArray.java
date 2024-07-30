9990f236b0758d51b7a8d33559d2fa3aec719434
Shreya Prakash <101544747+shreyaprakashh@users.noreply.github.com>
2024-07-30 23:06:10 +0530

    Create DAY16_SearchInRotatedSortedArray.java

---
+++ DAY16_SearchInRotatedSortedArray.java
@@ -0,0 +1,26 @@
+// There is an integer array nums sorted in ascending order (with distinct values).
+//Prior to being passed to your function, nums is possibly rotated at an unknown pivot index k (1 <= k < nums.length) such that the resulting array is [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed). For example, [0,1,2,4,5,6,7] might be rotated at pivot index 3 and become [4,5,6,7,0,1,2].
+// Given the array nums after the possible rotation and an integer target, return the index of target if it is in nums, or -1 if it is not in nums.
+// You must write an algorithm with O(log n) runtime complexity.
+
+class Solution {
+    public int search(int[] nums, int target) {
+      return find(nums,0,nums.length-1,target);
+    } 
+    public int find(int[]nums,int start,int end,int target){
+        if(start>end)return -1;
+        int mid=start+(end-start)/2;
+        if(nums[mid]==target)return mid;
+        if(nums[start]<=nums[mid]){
+            if(target>=nums[start]&&target<nums[mid]){
+                return find(nums,start,mid-1,target);
+            }else{
+                return find(nums,mid+1,end,target);
+            }
+        }
+        if(target>nums[mid]&&target<=nums[end]){
+            return find(nums,mid+1,end,target);
+        }
+        return find(nums,start,mid-1,target);
+    }
+}
