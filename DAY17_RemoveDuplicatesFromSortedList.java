3cbcac6640103b6b387754c31ddfdf8f15587cf7
Shreya Prakash <101544747+shreyaprakashh@users.noreply.github.com>
2024-07-31 23:19:13 +0530

    Create DAY17_RemoveDuplicatesFromSortedList.java

---
+++ DAY17_RemoveDuplicatesFromSortedList.java
@@ -0,0 +1,27 @@
+// Given the head of a sorted linked list, delete all duplicates such that each element appears only once. Return the linked list sorted as well.
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        if(head == null){
+            return null;
+        }
+        ListNode temp = head;
+        ListNode temp2 = head.next;
+        int last = head.val;
+        while(temp2 != null){ 
+            if(temp2.val == last){ 
+                if(temp2.next == null){ 
+                    temp.next = null;
+                    break;
+                }
+                temp2 = temp2.next; 
+                temp.next = temp2; 
+            }
+            else{ 
+                temp = temp2; 
+                last = temp.val;
+                temp2 = temp2.next;
+            }
+        }
+        return head;
+    }
+}
