e4919a239829fb564a1e1431daf4eaf1c26525f2
Shreya Prakash <101544747+shreyaprakashh@users.noreply.github.com>
2024-08-11 22:04:44 +0530

    Create DAY28_Binary Tree Paths.java

---
+++ DAY28_Binary Tree Paths.java
@@ -0,0 +1,24 @@
+// Given the root of a binary tree, return all root-to-leaf paths in any order.
+class Solution {
+    
+    ArrayList<String> list=new ArrayList<>();
+    public List<String> binaryTreePaths(TreeNode root) {
+        
+        String ans="";
+        path(root,ans);
+        return list;
+    }
+    void path(TreeNode root, String ans) {
+        if(root==null) return;
+        ans=ans+root.val;
+        if(root.left==null && root.right==null){
+            list.add(ans);
+            return;
+        }
+        ans=ans+"->";
+        path(root.left,ans);
+        path(root.right,ans);
+    }
+
+    
+}
