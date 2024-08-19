2356870bca14f620e44e092b9989f96180f2edb2
Shreya Prakash <101544747+shreyaprakashh@users.noreply.github.com>
2024-08-19 22:19:32 +0530

    Update DAY36_Number of Islands.java

--- DAY36_Number of Islands.java
+++ DAY36_Number of Islands.java
@@ -1,47 +1,49 @@
 // Given an m x n 2D binary grid grid which represents a map of '1's (land) and '0's (water), return the number of islands.
 class Solution {
-     private int[][] dir = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
-    
-    private boolean isSafe(char[][] grid, int i, int j) {
-        return i >= 0 && i < grid.length && j >= 0 && j < grid[0].length && grid[i][j] == '1';
+    class Pair {
+        public int first, second;
+
+        public Pair(int first, int second) {
+            this.first = first;
+            this.second = second;
+        }
     }
-    
-    private void bfs(char[][] grid, int i, int j, Queue<int[]> que) {
-        que.offer(new int[]{i, j});
-        grid[i][j] = '$';
-        
-        while (!que.isEmpty()) {
-            int[] curr = que.poll();
-            
-            for (int[] p : dir) {
-                int i_ = curr[0] + p[0];
-                int j_ = curr[1] + p[1];
-                
-                if (isSafe(grid, i_, j_)) {
-                    que.offer(new int[]{i_, j_});
-                    grid[i_][j_] = '$';
+
+    private void bfs(Pair node, char[][] grid, boolean[][] vis) {
+        Queue<Pair> queue = new LinkedList<Pair>();
+        int[][] dir = { { -1, 0 }, { 1, 0 }, { 0, -1 }, { 0, 1 } };
+        int n = grid.length, m = grid[0].length;
+        vis[node.first][node.second] = true;
+        queue.add(node);
+
+        while (!queue.isEmpty()) {
+            Pair node1 = queue.remove();
+
+            for (int[] d : dir) {
+                int row = node1.first + d[0];
+                int col = node1.second + d[1];
+
+                if (row >= 0 && row < n && col >= 0 && col < m && grid[row][col] == '1' && !vis[row][col]) {
+                    vis[row][col] = true;
+                    queue.add(new Pair(row, col));
                 }
             }
         }
     }
+
     public int numIslands(char[][] grid) {
-        if (grid.length == 0)
-            return 0;
-        
-        int m = grid.length;
-        int n = grid[0].length;
+        boolean[][] vis = new boolean[grid.length][grid[0].length];
         int count = 0;
-        Queue<int[]> que = new ArrayDeque<>();
-        
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (grid[i][j] == '1') {
-                    bfs(grid, i, j, que);
+        for (int row = 0; row < grid.length; row++) {
+            for (int col = 0; col < grid[0].length; col++) {
+                if (grid[row][col] == '1' && !vis[row][col]) {
                     count++;
+                    Pair node = new Pair(row, col);
+                    bfs(node, grid, vis);
                 }
             }
         }
-        
+
         return count;
     }
 }
