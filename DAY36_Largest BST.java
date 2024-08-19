06809d1766a98e847b4f218704c64302e175467e
Shreya Prakash <101544747+shreyaprakashh@users.noreply.github.com>
2024-08-19 21:38:34 +0530

    Create DAY36_Largest BST.java

---
+++ DAY36_Largest BST.java
@@ -0,0 +1,37 @@
+// Given a binary tree. Find the size of its largest subtree which is a Binary Search Tree.
+class NodeValue{
+    int maxVal,minVal,maxSize;
+    public NodeValue(int minVal,int maxVal,int maxSize){
+        this.minVal = minVal;
+        this.maxVal = maxVal;
+        this.maxSize = maxSize;
+    }
+};
+
+class Solution{
+    
+    // Return the size of the largest sub-tree which is also a BST
+    static NodeValue help(Node root){
+        if(root==null){
+            NodeValue temp = new NodeValue(Integer.MAX_VALUE,Integer.MIN_VALUE,0);
+            return temp;
+        }
+        NodeValue left = help(root.left);
+        NodeValue right = help(root.right);
+        if(left.maxVal<root.data&&root.data<right.minVal){
+            NodeValue temp = new NodeValue(Math.min(root.data,left.minVal),Math.max(root.data,right.maxVal),left.maxSize+
+            right.maxSize+1);
+            return temp;
+        }
+        NodeValue temp = new NodeValue(Integer.MIN_VALUE,Integer.MAX_VALUE,Math.max(left.maxSize,right.maxSize));
+        return temp;
+    }
+    
+    static int largestBst(Node root)
+    {
+        // Write your code here
+        return help(root).maxSize;
+        
+    }
+    
+}
