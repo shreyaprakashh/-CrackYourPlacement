48fc092985bc665722d3e188aa84e596dec62fc7
Shreya Prakash <101544747+shreyaprakashh@users.noreply.github.com>
2024-08-30 22:22:20 +0530

    Create DAY47_Maximum Product Subarray.java

---
+++ DAY47_Maximum Product Subarray.java
@@ -0,0 +1,19 @@
+// Given an integer array nums, find a subarray that has the largest product, and return the product.
+class Solution {
+    public int maxProduct(int[] nums) {
+        int n=nums.length;
+        int left=1;
+        int right=1;
+        int ans=nums[0];
+
+        for(int i=0;i<n;i++){
+            left=left==0?1:left;
+            right=right==0?1:right;
+            left*=nums[i];
+            right*=nums[n-1-i];
+
+            ans=Math.max(ans,Math.max(left,right));
+        }
+        return ans;
+    }
+}
