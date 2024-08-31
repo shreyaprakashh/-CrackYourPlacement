e30bdbeb90701a96b0f17b1bfbd19848235afaa5
Shreya Prakash <101544747+shreyaprakashh@users.noreply.github.com>
2024-08-31 22:23:11 +0530

    Create DAY48_Ones and Zeroes.java

---
+++ DAY48_Ones and Zeroes.java
@@ -0,0 +1,18 @@
+// You are given an array of binary strings strs and two integers m and n.
+// Return the size of the largest subset of strs such that there are at most m 0's and n 1's in the subset.
+
+class Solution {
+    public int findMaxForm(String[] strs, int m, int n) {
+        int[][] dp = new int[m+1][n+1];
+        for (String str : strs) {
+            int zeros = 0, ones = 0;
+            for (char c : str.toCharArray())
+                if (c == '0') zeros++;
+                else ones++;
+            for (int i = m; i >= zeros; i--)
+                for (int j = n; j >= ones; j--)
+                    dp[i][j] = Math.max(dp[i][j], dp[i-zeros][j-ones] + 1);
+        }
+        return dp[m][n];
+    }
+}
