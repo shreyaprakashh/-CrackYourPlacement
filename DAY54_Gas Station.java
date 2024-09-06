4d87ce50cfd7505acf08ca766c5b028d54977d77
Shreya Prakash <101544747+shreyaprakashh@users.noreply.github.com>
2024-09-06 23:47:02 +0530

    Create DAY54_Gas Station.java

---
+++ DAY54_Gas Station.java
@@ -0,0 +1,21 @@
+// Given two integer arrays gas and cost, return the starting gas station's index if you can travel around the circuit once in the clockwise direction, otherwise return -1. 
+// If there exists a solution, it is guaranteed to be unique.
+
+class Solution {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        int n = gas.length;
+        int total_surplus = 0;
+        int surplus = 0;
+        int start = 0;
+        
+        for(int i = 0; i < n; i++){
+            total_surplus += gas[i] - cost[i];
+            surplus += gas[i] - cost[i];
+            if(surplus < 0){
+                surplus = 0;
+                start = i + 1;
+            }
+        }
+        return (total_surplus < 0) ? -1 : start;
+    }
+}
