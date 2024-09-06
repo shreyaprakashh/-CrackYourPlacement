9bbfe3c9918988b1113efbe61855f6c1c2247224
Shreya Prakash <101544747+shreyaprakashh@users.noreply.github.com>
2024-09-06 23:42:59 +0530

    Create DAY54_Maximum Number of Visible Points.java

---
+++ DAY54_Maximum Number of Visible Points.java
@@ -0,0 +1,29 @@
+// You are given an array points, an integer angle, and your location, where location = [posx, posy] and points[i] = [xi, yi] both denote integral coordinates on the X-Y plane.
+// Return the maximum number of points you can see.
+
+class Solution {
+    public int visiblePoints(List<List<Integer>> points, int angle, List<Integer> location) {
+        List<Double> angles = new ArrayList<>();
+        int count = 0;
+        for (List<Integer> p : points) {
+            int dx = p.get(0) - location.get(0);
+            int dy = p.get(1) - location.get(1);
+            if (dx == 0 && dy == 0) { 
+                count++;
+                continue;
+            } 
+            angles.add(Math.atan2(dy, dx) * (180 / Math.PI));
+        }
+        Collections.sort(angles);
+        List<Double> tmp = new ArrayList<>(angles);
+        for (double d : angles) tmp.add(d + 360); 
+        int res = count;
+        for (int i = 0, j = 0; i < tmp.size(); i++) {
+            while (tmp.get(i) - tmp.get(j) > angle) {
+                j++;
+            }
+            res = Math.max(res, count + i - j + 1);
+        }
+        return res;
+    }
+}
