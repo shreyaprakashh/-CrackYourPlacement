6214c373d22e4af424522e4b6959cdc961fd7ec3
Shreya Prakash <101544747+shreyaprakashh@users.noreply.github.com>
2024-09-09 23:12:43 +0530

    Create DAY57_Combinations.java

---
+++ DAY57_Combinations.java
@@ -0,0 +1,25 @@
+// Given two integers n and k, return all possible combinations of k numbers chosen from the range [1, n].
+// You may return the answer in any order.
+
+class Solution {
+    public List<List<Integer>> combine(int n, int k) {
+        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> comb = new ArrayList<>();
+
+        backtrack(1, comb, res, n, k);
+        return res;
+    }
+
+    private void backtrack(int start, List<Integer> comb, List<List<Integer>> res, int n, int k) {
+        if (comb.size() == k) {
+            res.add(new ArrayList<>(comb));
+            return;
+        }
+
+        for (int num = start; num <= n; num++) {
+            comb.add(num);
+            backtrack(num + 1, comb, res, n, k);
+            comb.remove(comb.size() - 1);
+        }
+    } 
+}
