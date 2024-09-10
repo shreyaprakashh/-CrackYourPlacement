991e5ace51ec4190e789877146026841862e0fc9
Shreya Prakash <101544747+shreyaprakashh@users.noreply.github.com>
2024-09-10 21:50:44 +0530

    Create DAY58_Palindrome Partitioning.java

---
+++ DAY58_Palindrome Partitioning.java
@@ -0,0 +1,39 @@
+// Given a string s, partition s such that every substring of the partition is a palindrome. Return all possible palindrome partitioning of s.
+class Solution {
+    private int n;
+    
+    private boolean isPalindrome(String s, int l, int r) {
+        while (l < r) {
+            if (s.charAt(l) != s.charAt(r))
+                return false;
+            l++;
+            r--;
+        }
+        return true;
+    }
+    
+    private void backtrack(String s, int idx, List<String> curr, List<List<String>> result) {
+        if (idx == n) {
+            result.add(new ArrayList<>(curr));
+            return;
+        }
+        
+        for (int i = idx; i < n; i++) {
+            if (isPalindrome(s, idx, i)) {
+                curr.add(s.substring(idx, i + 1));
+                backtrack(s, i + 1, curr, result);
+                curr.remove(curr.size() - 1);
+            }
+        }
+    }
+    
+    public List<List<String>> partition(String s) {
+        n = s.length();
+        List<List<String>> result = new ArrayList<>();
+        List<String> curr = new ArrayList<>();
+        
+        backtrack(s, 0, curr, result);
+        
+        return result;
+    }
+}
